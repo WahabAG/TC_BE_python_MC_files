a685f61d7300673a082315bdfe10fa127d38562e
Azeez Wahab <azeezwahab24@gmail.com>
2025-11-06 22:01:35 +0100

    class encapsulation

--- 22_oop.py
+++ 22_oop.py
@@ -51,56 +51,124 @@
 # print(dog.height())
 
 # inheritance class task
-class Vehicle:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
+# class Vehicle:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
 
-class Car(Vehicle):
-    def __init__(self, brand, model, seats):
-        super().__init__(brand, model)
-        self.seats = seats
+# class Car(Vehicle):
+#     def __init__(self, brand, model, seats):
+#         super().__init__(brand, model)
+#         self.seats = seats
 
-    def car_details(self):
-        print(f"this is a {self.brand}")
-        print(f"Model: {self.model} with a 4 wheel drive and {self.seats} seats")
+#     def car_details(self):
+#         print(f"this is a {self.brand}")
+#         print(f"Model: {self.model} with a 4 wheel drive and {self.seats} seats")
 
-car = Car("Toyota", 2018, 6)
-car.car_details()
+# car = Car("Toyota", 2018, 6)
+# car.car_details()
 
 
-class Person:
-    def __init__(self, name, age):
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def race(self):
+#         race = "caucassian"
+#         return race
+
+# class Employee(Person):
+#     def __init__(self, name, age,  post, salary):
+#         super().__init__(name, age)
+#         self.post = post
+#         self.salary = salary
+
+#     def show_details(self):
+#         print(f" My Name is {self.name}, I am {self.age} years old")
+#         print(f" I work as a {self.post}, Earning a net of {self.salary} in dollars per month")
+
+#     def tax_payble(self):
+#         tax = self.salary * (9.5/100)
+#         return tax
+
+#     def race(self):         # polymorphisim of the race parent class
+#         race = "black"
+#         print(f"i am of african race which means i am {race}")
+
+# worker = Employee("Azeez", 28, "Backend Dev",50000)
+# Tax = worker.tax_payble()
+# worker.show_details()
+# print(f"My monthly income tax is: {Tax}")
+
+# worker.race()  #polymorphysim calling
+
+# class task
+# practicing poly morphism
+# class Calculation :
+#     def __init__(self, num1, num2):
+#         self.num1 = num1
+#         self.num2 = num2
+    
+#     def calculate(self):
+#         pass
+
+# class Adder(Calculation):
+
+#     def __init__(self, num1, num2):
+#         super().__init__(num1, num2)
+
+#     def calculate(self):
+#        return self.num1 + self.num2
+
+
+# class Multiplier(Calculation):
+
+#     def __init__(self, num1, num2):
+#         super().__init__(num1, num2)
+
+#     def calculate(self):
+#        return self.num1 * self.num2
+    
+
+# multiplication = Multiplier(15, 3)
+# mult_result = multiplication.calculate()
+# print(mult_result)
+
+# addition = Adder(15, 7)
+# add_result = addition.calculate()
+# print(add_result)
+
+
+class Employee:
+    def __init__(self, name, sex, salary):
         self.name = name
-        self.age = age
+        self.sex = sex
+        self._salary = salary
 
-    def race(self):
-        race = "caucassian"
-        return race
+    def get_salary(self):
+        return self._salary
 
-class Employee(Person):
-    def __init__(self, name, age,  post, salary):
-        super().__init__(name, age)
-        self.post = post
-        self.salary = salary
+    def set_salary(self, salary):
+        self._salary = salary
 
-    def show_details(self):
-        print(f" My Name is {self.name}, I am {self.age} years old")
-        print(f" I work as a {self.post}, Earning a net of {self.salary} in dollars per month")
 
-    def tax_payble(self):
-        tax = self.salary * (9.5/100)
-        return tax
 
-    def race(self):         # polymorphisim of the race parent class
-        race = "black"
-        print(f"i am of african race which means i am {race}")
+    def taxation(self):
+        tax = self._salary * (14/100)
+        print(tax)
 
-worker = Employee("Azeez", 28, "Backend Dev",50000)
-Tax = worker.tax_payble()
-worker.show_details()
-print(f"My monthly income tax is: {Tax}")
+    def apply_bonus(self):
+        self._salary = ((25/100) * self._salary) + self._salary
+        self._salary
 
-worker.race()  #polymorphysim calling
+staff = Employee("Azeez", "male", 200000)
 
+staff.taxation()
+print(staff.get_salary())
+staff.set_salary(500000)
+
+staff.taxation()
+print(staff.get_salary())
 # run this code by runing in thyour terminal ==> python 22_oop.py
+
--- 3_variables_numbers.py
+++ 3_variables_numbers.py
@@ -1,11 +1,19 @@
-x = -10        # int whole numbers
-y = -3.14     # float or decimals
-z = 1 + 2j   # complex special numbers mostly for formulars
+# x = -10        # int whole numbers
+# y = -3.14     # float or decimals
+# z = 1 + 2j   # complex special numbers mostly for formulars
 
 
-print(x)
-print(y)
-print(z)
+# print(x)
+# print(y)
+# print(z)
+
+
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
 
 # INSTRUCTIONS FOR TESTING
 # 1. Launch your terminal and run the code
