a7d3479decc1dab63ffff001ddb4c0e4e5646f6c
Azeez Wahab <azeezwahab24@gmail.com>
2025-11-07 07:33:58 +0100

    added advanced datatype file

---
+++ 23_advanced_data_types.py
@@ -0,0 +1,25 @@
+# list abd tupules
+# A list is a mutable (modifiable) collection of elements, it acn store multiple data types
+my_list = [1, 2, 3, "Apple", "red", True]
+# common operations
+# accessing elements
+
+print(my_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# to run the codes on this page coppy to your terminal ==> python 23_advanced_data_types.py
